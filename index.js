9bb91ba909b563c4836a35a22dce72cf3ed8a580
abdiaziz1795 <cabdulcazizc@gmail.com>
2025-03-17 10:57:55 +0300

    happy

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const button = document.getElementById("button"); // Ensure the element exists
+    button.addEventListener("click", () => {
+        console.log("Button was clicked!");
+    });
 }
+addingEventListener(); 
+
